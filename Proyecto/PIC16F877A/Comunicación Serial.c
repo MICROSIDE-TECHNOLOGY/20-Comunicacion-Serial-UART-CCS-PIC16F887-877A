f35328caea4afd70a18c01caf3be8252d365b698
vhperaltaj <victor.peralta@microside.com>
2023-07-12 17:24:25 -0600

    Initial commit

---
+++ Proyecto/PIC16F877A/Comunicación Serial.c
@@ -0,0 +1,53 @@
+/*
+ AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
+ FECHA: JUNIO 2019
+*/
+
+/*
+------------------------------------------------------------------------------
+ Implementar comunicación serial para enviar un comando simple para prender y
+ apagar un LED incluido en la tarjeta X-TRAINER, y un segundo comando para
+ preguntar el estado de un botón.
+------------------------------------------------------------------------------
+*/
+
+#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
+#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+
+
+#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7,STREAM=UART ) //Configuración del puerto UART
+
+#define LED PIN_A1                                              //Pin donde está conectado el LED del X-TRAINER
+#define Boton PIN_A2                                            //Pin donde está conectado el BOTON del X-TRAINER
+
+void main ()
+
+  {
+   while (1)
+  {
+
+   char Caracter = getc ();                         //Guarda el caracter
+   if (Caracter == '0')
+    {
+      output_low (LED);                             //Apaga el LED
+    }
+    
+     else if (Caracter == '1')
+    {
+     output_HIGH (LED);                             //Enciende el LED
+    }
+    
+     else if (Caracter == '?')
+    {
+        if (1 == input (Boton))                     //Pregunta el estado del botón
+          {                                                                   
+            Printf ("0");                           //Envía un 0 si el botón no está presionado
+          }
+          
+          else
+         {
+           Printf ("1");                            //Envía un 1 si el botón está presionado
+         }
+      }
+   }
+}
