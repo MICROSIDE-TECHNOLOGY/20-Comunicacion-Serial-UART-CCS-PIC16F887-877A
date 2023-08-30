cf471bed14bdb179c9be5c3e074776ac4c3f07bb
vhperaltaj <victor.peralta@microside.com>
2023-08-30 12:03:00 -0600

    Updated identantion, added gitignore

--- Proyecto/PIC16F877A/Comunicación Serial.c
+++ Proyecto/PIC16F877A/Comunicación Serial.c
@@ -1,7 +1,12 @@
-/*
- AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
- FECHA: JUNIO 2019
-*/
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+File Name:
+Comunicación Serial.c
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 
 /*
 ------------------------------------------------------------------------------
@@ -11,42 +16,29 @@
 ------------------------------------------------------------------------------
 */
 
-#include <16F877A.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#use delay(clock=20Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#include <16F877A.h>                  //Incluye el microcontrolador con el que se va a trabajar
+#use delay( clock = 20Mhz, crystal )  // Tipo de oscilador y frecuencia dependiendo del microcontrolador
 
+#use rs232( baud = 9600, xmit = PIN_C6, rcv = PIN_C7, STREAM = UART ) // Configuración del puerto UART
 
-#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7,STREAM=UART ) //Configuración del puerto UART
+#define LED PIN_A1                    // Pin donde está conectado el LED del X-TRAINER
+#define Boton PIN_A2                  // Pin donde está conectado el BOTON del X-TRAINER
 
-#define LED PIN_A1                                              //Pin donde está conectado el LED del X-TRAINER
-#define Boton PIN_A2                                            //Pin donde está conectado el BOTON del X-TRAINER
+void main( void ) {
 
-void main ()
+   while ( 1 ) {
+      char Caracter = getc();         // Guarda el caracter
+      
+      if ( Caracter == '0' ) {
+         output_low( LED );           // Apaga el LED
+      } else if ( Caracter == '1' ) {
+         output_HIGH( LED );          // Enciende el LED
+      } else if ( Caracter == '?' ) {
 
-  {
-   while (1)
-  {
-
-   char Caracter = getc ();                         //Guarda el caracter
-   if (Caracter == '0')
-    {
-      output_low (LED);                             //Apaga el LED
-    }
-    
-     else if (Caracter == '1')
-    {
-     output_HIGH (LED);                             //Enciende el LED
-    }
-    
-     else if (Caracter == '?')
-    {
-        if (1 == input (Boton))                     //Pregunta el estado del botón
-          {                                                                   
-            Printf ("0");                           //Envía un 0 si el botón no está presionado
-          }
-          
-          else
-         {
-           Printf ("1");                            //Envía un 1 si el botón está presionado
+         if ( 1 == input( Boton ) ) { // Pregunta el estado del botón
+            Printf( "0" );            // Envía un 0 si el botón no está presionado
+         } else {
+            Printf( "1" );            // Envía un 1 si el botón está presionado
          }
       }
    }
